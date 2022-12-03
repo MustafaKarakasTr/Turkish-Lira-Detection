36722208ea13248698cb1676e8da2bcf1542854d
MustafaKarakasTr <mustafa.krks.09@gmail.com>
2022-12-03 23:56:40 +0300

    realtime detection branch initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image Classification',
+      title: 'Turkish Lira Detection',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
